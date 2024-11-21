7eef401caee18541fbe7aedc803762f2082ca492
User <User@WIN-KDN4JH014R1>
2024-11-21 20:29:29 +0300

    Добавьте файлы проекта.

---
+++ Домашняя работа 3/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Задание 1. Дана последовательность из 10 чисел. Определить, является ли эта последовательность
+            упорядоченной по возрастанию.В случае отрицательного ответа определить
+            порядковый номер первого числа, которое нарушает данную последовательность. Использовать break. */
+            Console.WriteLine("1");
+            int[] numbers = new int[10];
+            Console.WriteLine("Введите 10 чисел:");
+            for (int a = 0; a < 10; a++)
+            {
+                Console.Write($"Число {a + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out numbers[a]))
+                {
+                    Console.Write("Ошибка! Введите целое число еще раз: ");
+                }
+            }
+            bool ordered = true;
+            int mistake = -1;
+
+            for (int a = 0; a < numbers.Length - 1; a++)
+            {
+                if (numbers[a] > numbers[a + 1])
+                {
+                    ordered = false;
+                    mistake = a + 1; // +1, чтобы индекс соответствовал порядковому номеру
+                    break;
+                }
+            }
+            if (ordered)
+            {
+                Console.WriteLine("Последовательность является упорядоченной по возрастанию.");
+            }
+            else
+            {
+                Console.WriteLine($"Последовательность неупорядочена. Первое число, нарушающее последовательность: {mistake + 1}");
+            }
+
+            /* Задание 2.Игральным картам условно присвоены следующие порядковые номера в зависимости от
+        их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+        Порядковые номера остальных карт соответствуют их названиям («шестерка», «девятка» и т. п.).
+        По заданному номеру карты k (6 <=k <= 14) определить достоинство соответствующей карты. Использовать try-catch-finally. */
+            Console.WriteLine("2");
+
+            Console.WriteLine("Введите порядковый номер карты (от 6 до 14):");
+            string input = Console.ReadLine();
+            int k;
+
+            try
+            {
+                k = int.Parse(input);
+                if (k < 6 || k > 14)
+                {
+                    throw new ArgumentOutOfRangeException("Картам соответствуют номера только от 6 до 14.");
+                }
+                string cardValue;
+
+                switch (k)
+                {
+                    case 6:
+                        cardValue = "Шестёрка";
+                        break;
+                    case 7:
+                        cardValue = "Семёрка";
+                        break;
+                    case 8:
+                        cardValue = "Восьмёрка";
+                        break;
+                    case 9:
+                        cardValue = "Девятка";
+                        break;
+                    case 10:
+                        cardValue = "Десятка";
+                        break;
+                    case 11:
+                        cardValue = "Валет";
+                        break;
+                    case 12:
+                        cardValue = "Дама";
+                        break;
+                    case 13:
+                        cardValue = "Король";
+                        break;
+                    case 14:
+                        cardValue = "Туз";
+                        break;
+                    default:
+                        cardValue = "Неизвестная карта";
+                        break;
+                }
+
+                Console.WriteLine($"Достоинство карты: {cardValue}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверно введено целое число!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Программа завершена.");
+            }
+
+            /* Задание 4. Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,...,7)
+            выводит на экран его название (понедельник, вторник, ..., воскресенье).Использовать enum. */
+            Console.WriteLine("4");
+            try
+            {
+                Console.WriteLine("Введите порядковый номер дня недели (1 - 7):");
+                int dayNumber = int.Parse(Console.ReadLine());
+                if (dayNumber < 1 || dayNumber > 7)
+                {
+                    throw new ArgumentOutOfRangeException("Введите корректный номер дня недели (от 1 до 7).");
+                }
+
+                DayOfWeek dayOfWeek = (DayOfWeek)dayNumber;
+
+                switch (dayOfWeek)
+                {
+                    case DayOfWeek.Monday:
+                        Console.WriteLine("Сегодня понедельник.");
+                        break;
+                    case DayOfWeek.Tuesday:
+                        Console.WriteLine("Сегодня вторник.");
+                        break;
+                    case DayOfWeek.Wensday:
+                        Console.WriteLine("Сегодня среда.");
+                        break;
+                    case DayOfWeek.Thursday:
+                        Console.WriteLine("Сегодня четверг.");
+                        break;
+                    case DayOfWeek.Friday:
+                        Console.WriteLine("Сегодня пятница.");
+                        break;
+                    case DayOfWeek.Saturday:
+                        Console.WriteLine("Сегодня суббота.");
+                        break;
+                    case DayOfWeek.Sunday:
+                        Console.WriteLine("Сегодня воскресенье.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введите корректное целое число.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wensday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ Лабораторная№4/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Лабораторная 4.1. Написать программу, которая читает с экрана число от 1 до 365 (номер дня в году),
+         переводит этот число в месяц и день месяца.Например, число 40 соответствует 9 февраля(високосный год не учитывать).*/
+            Console.WriteLine("Лабораторная 4.1");
+            Console.Write("Введите номер дня в году (от 1 до 365): ");
+            int dayOfYear;
+            while (!int.TryParse(Console.ReadLine(), out dayOfYear) || dayOfYear < 1 || dayOfYear > 365)
+            {
+                Console.WriteLine("Пожалуйста, введите корректное число от 1 до 365.");
+            }
+            DateTime Date = new DateTime(2023, 1, 1).AddDays(dayOfYear - 1);
+            Console.WriteLine($"Номер дня {dayOfYear} соответствует дате: {Date.ToString("d MMMM")}");
+
+
+            /* Лабораторная 4.2. Добавить к задаче из предыдущего упражнения проверку числа введенного
+            пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать 
+            исключение, и выдавать на экран сообщение. */
+            Console.WriteLine("Лабораторная 4.2");
+            try
+            {
+                Console.Write("Введите номер дня в году (от 1 до 365): ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int day))
+                {
+                    if (day < 1 || day > 365)
+                    {
+                        throw new ArgumentOutOfRangeException("day", "Номер дня должен быть в диапазоне от 1 до 365.");
+                    }
+                    DateTime date = new DateTime(2023, 1, 1).AddDays(day - 1);
+                    Console.WriteLine($"Номер дня {day} соответствует дате: {date.ToString("d MMMM")}");
+                }
+                else
+                {
+                    throw new FormatException("Введите корректное целое число.");
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+
+            }
+
+            /* Домашнее задание 4.1. Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
+            учитывала год (високосный или нет). Год вводится с экрана. (Год високосный, если он 
+            делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный год.
+            Однако, если он делится без остатка на 400, это високосный год.) */
+            Console.WriteLine("Домашнее задание 4.1");
+            try
+            {
+                Console.Write("Введите год: ");
+                string Year = Console.ReadLine();
+                if (!int.TryParse(Year, out int year))
+                {
+                    throw new FormatException("Введите год корректно!");
+                }
+                Console.Write("Введите номер дня в году (от 1 до 365): ");
+                string Day = Console.ReadLine();
+                if (!int.TryParse(Day, out int day))
+                {
+                    throw new FormatException("Введите корректное число.");
+                }
+
+                if (day < 1 || day > 365)
+                {
+                    throw new ArgumentOutOfRangeException("day", "Номер дня должен быть в диапазоне от 1 до 365.");
+                }
+                bool LeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);// Проверяем високосный год
+
+                DateTime date = new DateTime(year, 1, 1).AddDays(day - 1);
+                if (LeapYear && day > 59) // 59(28 февраля в обычном году)
+                {
+                    date = date.AddDays(1); // Добавляем еще один день для високосного года
+                }
+
+                // Выводим результат
+                Console.WriteLine($"Номер дня в году соответствует дате: {date.ToString("d MMMM yyyy")}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
